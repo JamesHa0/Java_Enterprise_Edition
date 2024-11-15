60660d2e67f394dd0ea1ab182e9a25fc229c28fe
JamesHa0 <2258693275@qq.com>
2024-11-15 15:03:41 +0800

    2024.11.15下午

--- JavaWeb/demo1114/src/main/java/com/jameshao/demo1114/servlet/FirstServlet.java
+++ JavaWeb/demo1114/src/main/java/com/jameshao/demo1114/servlet/FirstServlet.java
@@ -1,11 +1,16 @@
 package com.jameshao.demo1114.servlet;
 
+import com.alibaba.fastjson2.JSON;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(value = "/first")
 public class FirstServlet extends HttpServlet {
@@ -16,14 +21,27 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 向Request作用域中添加属性
-        req.setAttribute("message","Message信息");
 
-        //向Session作用域中添加属性
-        req.getSession().setAttribute("message","session信息");
+        System.out.println("访问了FirstServlet");
+        List<String> data = new ArrayList<>();
+        data.add("Hallo...");
+        data.add("xxx...");
 
-        //向ServletContext作用域中添加属性
-        req.getServletContext().setAttribute("message","servletcontext信息");
+        PrintWriter out = resp.getWriter();
+        out.println(JSON.toJSONString(data));
+        out.close();
+
+//        // 向Request作用域中添加属性
+//        req.setAttribute("message","Message信息");
+//
+//        //向Session作用域中添加属性
+//        req.getSession().setAttribute("message","session信息");
+//
+//        //向ServletContext作用域中添加属性
+//        req.getServletContext().setAttribute("message","servletcontext信息");
+
+
+
 
     }
 }
